b8bf19d5e5aad87629761e626a449a3a03cd81d7
Hossein Fatemi <hfatemi1997@gmail.com>
2023-12-23 18:32:31 +0330

    INIT: initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hi Guys</h1>
+      <h2>My Name is Hossein Fatemi</h2>
+      <h3>Swagger Auto generator</h3>
+    </>
+  );
+}
+
+export default App;
